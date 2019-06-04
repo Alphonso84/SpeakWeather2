0cf44d5ab9f222ff85e7cdb04e10f35afb208251
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-06-03 19:32:49 -0700

    Updating project

---
+++ SpeakWeather2/HourlyTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyTableViewCell.swift
+//  SpeakWeather2
+//
+//  Created by user on 6/2/19.
+//  Copyright © 2019 Alphonso Sensley II. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+    
+    var timeLabel = UILabel()
+    var weatherImage = UIImageView()
+    var temp = UILabel()
+}
---
+++ SpeakWeather2/TableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TableViewController.swift
+//  SpeakWeather2
+//
+//  Created by user on 6/3/19.
+//  Copyright © 2019 Alphonso Sensley II. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class tableViewController: UITableView, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+    
+    
+}
