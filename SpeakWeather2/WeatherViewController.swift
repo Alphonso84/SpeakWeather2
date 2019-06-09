5aefbdb62d0e5467330e7b0605efe64cb8bf7475
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-06-08 18:37:55 -0700

    Adding Model for weather data

--- SpeakWeather2/WeatherViewController.swift
+++ SpeakWeather2/WeatherViewController.swift
@@ -18,7 +18,7 @@ struct City {
 var favoriteCities = [City]()
 
 class WeatherViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
     var wasSwitched = false
     var Hourly = [Date]()
@@ -29,23 +29,23 @@ class WeatherViewController: UIViewController, UICollectionViewDelegate, UIColle
     @IBAction func HourOrWeek(_ sender: Any) {
         tableView.reloadData()
         wasSwitched = !wasSwitched
-     print("\(wasSwitched)")
+        print("\(wasSwitched)")
         returnTableRows()
-            
-   }
+        
+    }
     
     
     @IBOutlet weak var weatherImage: UIImageView!
     
     @IBOutlet weak var collectionView: UICollectionView!
-   
+    
     
     let SanFrancisco = City(name: "San Francisco", condition: "Partly Cloudy", temp: "54", image: UIImage(named: "SF")!)
     let Oakland = City(name: "Oakland", condition: "Sunny", temp: "70", image: UIImage(named: "Oakland")!)
     let Antioch = City(name: "Antioch", condition: "Sunny", temp: "76", image: UIImage(named:"Antioch")!)
     let SanLeandro = City(name: "San Leandro", condition: "Cloudy", temp: "56", image: UIImage(named: "SAN LEANDRO")!)
     let cellScaling: CGFloat = 0.6
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return favoriteCities.count
     }
@@ -60,24 +60,24 @@ class WeatherViewController: UIViewController, UICollectionViewDelegate, UIColle
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-       
+        
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         print(scrollView.contentOffset.x)
-//  Changing the backGroundImage based on where the user scrolls.
+        //  Changing the backGroundImage based on where the user scrolls.
         let offset = scrollView.contentOffset.x
         
         if offset < 216.5 {
             //backgroundImageView.image = favoriteCities[0].image
         } else if offset < 433 {
-           // backgroundImageView.image = favoriteCities[1].image
+            // backgroundImageView.image = favoriteCities[1].image
         } else if offset < 649.5 {
-           // backgroundImageView.image = favoriteCities[2].image
+            // backgroundImageView.image = favoriteCities[2].image
         } else if offset < 866 {
-           // backgroundImageView.image = favoriteCities[3].image
+            // backgroundImageView.image = favoriteCities[3].image
         } else if offset >= 866 {
-           // backgroundImageView.image = favoriteCities[3].image
+            // backgroundImageView.image = favoriteCities[3].image
         }
         
     }
@@ -92,20 +92,23 @@ class WeatherViewController: UIViewController, UICollectionViewDelegate, UIColle
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return returnTableRows()
+        return returnTableRows()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         if wasSwitched == true{
-        cell.textLabel?.text = "Week"
-        }else{
+            cell.textLabel?.text = "Week"
+        } else {
             cell.textLabel?.text = "Today"
         }
         return cell
     }
-    
-    
+   
+//    func addFavoriteCity(name:String) ->City {
+//
+//        return City
+//    }
     
     override func viewWillAppear(_ animated: Bool) {
         favoriteCities.append(SanFrancisco)
